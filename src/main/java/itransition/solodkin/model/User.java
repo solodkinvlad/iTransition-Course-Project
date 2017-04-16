75a910e5cff4ccfd5300e86ccec812ce3995ec52
Vlad <Solodkinvlad97@mail.ru>
2017-04-16 19:16:39 +0300

    Repaired some shit (2).

--- src/main/java/itransition/solodkin/model/User.java
+++ src/main/java/itransition/solodkin/model/User.java
@@ -8,6 +8,7 @@ import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
@@ -28,11 +29,12 @@ public class User {
     @NotEmpty
     private String password;
 
-    @NotEmpty
+    @NotNull
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
     private String name;
+
     private Date birthday;
     private double weight;
     private double height;
@@ -40,5 +42,12 @@ public class User {
 
     public User(String name) {
         this.name = name;
+    }
+
+    public User(String name, UserRole userRole, String password, String email) {
+        this.name = name;
+        this.userRole = userRole;
+        this.password = password;
+        this.email = email;
     }
 }
--- src/main/java/itransition/solodkin/model/UserRole.java
+++ src/main/java/itransition/solodkin/model/UserRole.java
@@ -4,6 +4,16 @@ package itransition.solodkin.model;
  * Created by Влад on 12.04.2017.
  */
 public enum UserRole {
-    ROLE_ADMIN,
-    ROLE_USER
+    ROLE_ADMIN("Admin"),
+    ROLE_USER("User");
+
+    private final String label;
+
+    UserRole(String role) {
+        this.label = role;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
