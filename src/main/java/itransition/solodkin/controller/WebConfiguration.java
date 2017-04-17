574ace9332ddf9ca8469289df843ff3c04e5c5df
Emil <e.abilov@hotmail.com>
2017-04-17 19:36:40 +0300

    registration page was added

--- src/main/java/itransition/solodkin/controller/WebConfiguration.java
+++ src/main/java/itransition/solodkin/controller/WebConfiguration.java
@@ -11,6 +11,8 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("home");
         registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/registration").setViewName("registration");
     }
 }
