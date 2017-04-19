631e3f851db8fa47217c8d2916ad3f91578b517b
Emil <e.abilov@hotmail.com>
2017-04-19 18:40:26 +0300

    facebook authentication was added troubles with User model

--- src/main/java/itransition/solodkin/security/SecurityConfiguration.java
+++ src/main/java/itransition/solodkin/security/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package itransition.solodkin.security;
 
+import itransition.solodkin.service.FacebookConnectionSignUp;
+import itransition.solodkin.service.FacebookConnectionSignup;
+import itransition.solodkin.service.FacebookSignInAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +15,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInController;
 
 /**
  * Created by Влад on 12.04.2017.
@@ -39,7 +46,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll()
                 .and()
-                .exceptionHandling().accessDeniedPage("/login");
+                .exceptionHandling().accessDeniedPage("/login")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login*","/signin/**","/signup/**").permitAll();
     }
 
     @Autowired
@@ -59,6 +69,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private ConnectionFactoryLocator connectionFactoryLocator;
+
+    @Autowired
+    private UsersConnectionRepository usersConnectionRepository;
+
+    @Autowired
+    private FacebookConnectionSignUp facebookConnectionSignup;
+
+
+    @Bean
+    public ProviderSignInController providerSignInController() {
+        ((InMemoryUsersConnectionRepository) usersConnectionRepository)
+                .setConnectionSignUp(facebookConnectionSignup);
+
+        return new ProviderSignInController(
+                connectionFactoryLocator,
+                usersConnectionRepository,
+                new FacebookSignInAdapter());
     }
 
 }
