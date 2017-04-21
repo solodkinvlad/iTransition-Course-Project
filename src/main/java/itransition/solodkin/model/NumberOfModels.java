3362ca98e027410c14486d3b8a0ffa147f3a5f4b
Emil <e.abilov@hotmail.com>
2017-04-21 22:24:30 +0300

    profile_settings. filmings and number of models were added

--- src/main/java/itransition/solodkin/model/NumberOfModels.java
+++ src/main/java/itransition/solodkin/model/NumberOfModels.java
@@ -1,8 +1,11 @@
 package itransition.solodkin.model;
 
+import lombok.Getter;
+
 /**
  * Created by eabil on 21.04.2017.
  */
+@Getter
 public enum NumberOfModels {
     SINGLE("Single"),
     PAIRED("Paired"),
