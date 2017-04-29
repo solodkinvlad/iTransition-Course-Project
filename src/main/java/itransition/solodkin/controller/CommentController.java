25488921729f15241c087fd32c295cf63dea6557
Vlad <Solodkinvlad97@mail.ru>
2017-04-29 17:51:21 +0300

    Repaired after merge

--- src/main/java/itransition/solodkin/controller/CommentController.java
+++ src/main/java/itransition/solodkin/controller/CommentController.java
@@ -2,27 +2,28 @@ package itransition.solodkin.controller;
 
 import itransition.solodkin.model.CloudPhoto;
 import itransition.solodkin.model.Comment;
-import itransition.solodkin.security.SecurityServiceImpl;
+import itransition.solodkin.security.SecurityService;
 import itransition.solodkin.service.CloudphotoService;
 import itransition.solodkin.service.UserService;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by eabil on 29.04.2017.
- */
 @Controller
 public class CommentController {
     private CloudphotoService cloudphotoService;
     private UserService userService;
+    private SecurityService securityService;
 
     @Autowired
-    public CommentController(CloudphotoService cloudphotoService, UserService userService) {
+    public CommentController(CloudphotoService cloudphotoService, UserService userService, SecurityService securityService) {
         this.cloudphotoService = cloudphotoService;
         this.userService = userService;
+        this.securityService = securityService;
     }
 
     @GetMapping(value = "/comment")
@@ -31,8 +32,8 @@ public class CommentController {
     JSONArray addComment(@RequestParam String photoId, @RequestParam String text) {
         CloudPhoto photo = this.cloudphotoService.findOne(Long.parseLong(photoId));
         Comment comment = new Comment();
-        comment.setUserId(SecurityServiceImpl.getUserId());
-        comment.setNickname(this.userService.findOne(SecurityServiceImpl.getUserId()).getProfile().getNickname());
+        comment.setUserId(securityService.getUserId());
+        comment.setNickname(this.userService.findOne(securityService.getUserId()).getProfile().getNickname());
         comment.setComment(text);
         photo.getComments().add(comment);
         this.cloudphotoService.save(photo);
