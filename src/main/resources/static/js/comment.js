dc15ae976fca01de081e9a714bc920f9b32dd241
Emil <e.abilov@hotmail.com>
2017-04-30 18:41:39 +0300

    ez comments

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -7,22 +7,22 @@ function comment(element) {
     $.ajax({
         url: '/comment',
         type: 'GET',
-        data: {'photoId' : photoId, 'text' : text},
+        data: {'photoId': photoId, 'text': document.getElementById(photoId + 'textId').value},
         success: function (response) {
-            $(document).ready(function(){
-                document.getElementById('comments').innerHTML = '';
-                var i=0;
-                var str = '';
-                while (i < response.length - 1) {
-                    var nickname = response[i];
-                    var comment = response[i+1];
-                    str = str +  '<div> <p>'+nickname+'</p> <p>'+comment+'</p> </div>';
-                    i = i + 2;
-                }
+             $(document).ready(function () {
+             document.getElementById(photoId + 'comments').innerHTML = '';
+             var i = 0;
+             var str = '';
+             while (i < response.length - 1) {
+             var nickname = response[i];
+             var comment = response[i + 1];
+             str = str + '<div> <p>' + nickname + '</p> <p>' + comment + '</p> </div>';
+             i = i + 2;
+             }
 
-                document.getElementById('comments').innerHTML = str;
-                document.getElementById('textId').value='';
-            });
+             document.getElementById(photoId + 'comments').innerHTML = str;
+             document.getElementById(photoId + 'textId').value = '';
+             });
 
         }
     });
