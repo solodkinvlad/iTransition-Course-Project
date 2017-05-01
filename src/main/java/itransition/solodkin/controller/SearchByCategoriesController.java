06dfebfe09ade677f0d46bd192ae087ae5c306b0
Emil <e.abilov@hotmail.com>
2017-05-02 00:24:50 +0300

    repair search by categories

--- src/main/java/itransition/solodkin/controller/SearchByCategoriesController.java
+++ src/main/java/itransition/solodkin/controller/SearchByCategoriesController.java
@@ -3,6 +3,7 @@ package itransition.solodkin.controller;
 import itransition.solodkin.model.*;
 import itransition.solodkin.service.ProfileService;
 import itransition.solodkin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import java.util.List;
 public class SearchByCategoriesController {
     private ProfileService profileService;
 
+    @Autowired
     public SearchByCategoriesController(ProfileService profileService) {
         this.profileService = profileService;
     }
@@ -36,15 +38,24 @@ public class SearchByCategoriesController {
     @PostMapping("/search_by_categories")
     private String search(ProfileSearch profile, Model model) {
         List<Profile> result = new ArrayList<>();
-        List<Profile> profiles = this.profileService.findAll();
-        for (Profile prof : profiles) {
-            if ((prof.getFilmingTypes().containsAll(profile.getFilmingTypes())) &&
-                    (profile.getGenders().contains(prof.getGender())) &&
-                    (prof.getNumberOfModels().containsAll(profile.getNumberOfModels()))){
-                result.add(prof);
+        if (profile.getGenders().size() != 0
+                && profile.getFilmingTypes().size() != 0
+                && profile.getNumberOfModels().size() != 0) {
+            List<Profile> profiles = this.profileService.findAll();
+            for (Profile prof : profiles) {
+                if ((prof.getFilmingTypes().containsAll(profile.getFilmingTypes())) &&
+                        (profile.getGenders().contains(prof.getGender())) &&
+                        (prof.getNumberOfModels().containsAll(profile.getNumberOfModels()))) {
+                    result.add(prof);
+                }
             }
         }
         model.addAttribute("profiles", result);
+        ProfileSearch profileSearch = new ProfileSearch();
+        model.addAttribute("profile", profileSearch);
+        model.addAttribute("genders", Arrays.asList(Gender.values()));
+        model.addAttribute("filmingTypes", Arrays.asList(FilmingType.values()));
+        model.addAttribute("numberOfModels", Arrays.asList(NumberOfModels.values()));
         return "/search_profile_by_categories";
     }
 }
